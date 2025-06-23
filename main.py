336bacdc7fbd8c8864df84e81d9c0ee49040944a
walther_knight <knightofargh@gmail.com>
2025-06-23 15:25:46 -0400

    added prompt argument handling

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <"prompt">. Retry with proper syntax.')
+        sys.exit(1)
 
-response = client.models.generate_content(
-    model= "gemini-2.0-flash-001", contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-print(f"Response: {response.text}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    prompt_value = sys.argv[1]
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model= "gemini-2.0-flash-001", contents= prompt_value
+        )
+    print(f"Response: {response.text}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
